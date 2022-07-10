3421944bc67d4c7048a2d4fd0279c37074ab8537
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 00:11:58 +0100

    Update blind-ssrf-headers.sh

--- ssrf/blind-ssrf-headers/blind-ssrf-headers.sh
+++ ssrf/blind-ssrf-headers/blind-ssrf-headers.sh
@@ -12,4 +12,4 @@ server=$2
 server_fix=$(echo $server | sed -r "s/https?:\/\///")
 
 echo -e "${RED}CHECK YOUR SERVER FOR POTENTIALS CALLBACKS!${NC}"
-ffuf -v -w $list:W1,headers.txt:W2 -H "W2: $server_fix" -u W1 2 > /dev/null
+ffuf -r -w $list:W1,headers.txt:W2 -H "W2: $server_fix" -u W1 2 > /dev/null
