5a48cf8ca49c7dce2184af2a2dfaf8803b4b3b40
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-10 12:54:19 -0400

    Add files via upload

---
+++ ssrf/ssrf-headers/README.md
@@ -0,0 +1,7 @@
+# Fuzzing Headers for SSRF
+
+Bash script for fuzzing headers for SSRF by providing a list of URL's using ffuf
+
+Example usage: ./ssrf-headers urls.txt
+
+![image](https://user-images.githubusercontent.com/80685782/178123765-ba69c1c3-6c3a-4778-9d66-ecdf9c9cb658.png)
---
+++ ssrf/ssrf-headers/ssrf-headers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Coded by @lemosnlmb
+
+RED='\033[1;31m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+list=$1
+
+ffuf -v -w $list:W1,headers.txt:W2,payloads.txt:W3 -H 'W2: W3' -u W1 -mr "compute.internal" -s | while read url
+do
+	echo -e "${RED}Vulnerable to SSRF${NC}: $url" |
+	awk -v srch="W1 :" -v repl="${CYAN}URL${NC}->" '{ sub(srch,repl,$0); print $0 }' | awk -v srch="W2 :" -v repl="${CYAN}HEADER${NC}->" '{ sub(srch,repl,$0); print $0 }' | awk -v srch="W3 :" -v repl="${CYAN}PAYLOAD${NC}->" '{ sub(srch,repl,$0); print $0 }'
+done
