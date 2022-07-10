ef83719673db80aa36db52e4c8dd3da12b692c8f
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-10 12:48:52 -0400

    Add files via upload

---
+++ rce/rce-params/README.md
@@ -0,0 +1,7 @@
+# Fuzzing Parameters for RCE
+
+Bash script for fuzzing parameters for RCE by providing a URL's list using ffuf
+
+Example usage: ./ssrf-param.sh urls.txt 
+
+![image](https://user-images.githubusercontent.com/80685782/178129493-f4f65113-6f0c-4704-aac8-9c812fa3fff5.png)
---
+++ rce/rce-params/rce-params.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Coded by @lemosnlmb
+
+#Colors
+RED='\033[1;31m'
+NC='\033[0m'
+
+list=$1
+
+cat $list | grep "=" | qsreplace "FUZZ" > $list.tmp && mv $list.tmp $list
+interlace -tL $list -c "ffuf -v -r -w payloads.txt -u \"_target_\" -mr \".*uid=.*gid=.*\" " 2>/dev/null | grep "| URL |" | awk '{print $4}' | while read url; do echo -e "${RED}Vulnerable to RCE${NC}: $url"; done
