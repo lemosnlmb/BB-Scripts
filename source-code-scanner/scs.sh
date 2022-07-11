4f7ff2b3cbb716a98869adb9576a8c0e4c7394de
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 13:27:13 +0100

    Update scs.sh

--- source-code-scanner/scs.sh
+++ source-code-scanner/scs.sh
@@ -9,9 +9,30 @@ NC='\033[0m'
 
 #Usage
 printusage(){
-    echo -e "${CYAN} ${BOLD}"
+    echo -e "${CYAN}"
     echo -e "Example Usage: ${NC}"
     echo -e "./scs -l targets.txt"
+    exit
+}
+
+
+#Usage
+printusage(){
+    echo -e "${CYAN}"
+    echo -e "Required Flag: ${NC}"
+    echo -e "   -l, --list              		${YELLOW}Add URL's to scan${NC}"
+    echo -e "${CYAN}"
+    echo -e "Scan modes: ${NC}"
+    echo -e "   -g, --git               		${YELLOW}Scan for exposed git folders${NC}"
+    echo -e "   -b, --bazaar            		${YELLOW}Scan for exposed Bazaar folders${NC}"
+    echo -e "   -m, --mercury           		${YELLOW}Scan for exposed Mercury folders${NC}"
+    echo -e "   -s, --svn               		${YELLOW}Scan for exposed svn folders${NC}"
+    echo -e "   -a, --all               		${YELLOW}Scan for all exposed folders${NC}"
+    echo -e "${CYAN}"
+    echo -e "Examples: ${NC}"
+    echo -e "   ./scs.sh -l urls.txt -g -b -s        ${YELLOW}Scanning for git, Bazaar and svn folders${NC}"
+    echo -e "   ./scs.sh -l urls.txt -m              ${YELLOW}Scanning only for Mercury folders${NC}"
+    echo -e "   ./scs.sh -l urls.txt -a              ${YELLOW}Scanning for all folders${NC}"
     exit
 }
 
@@ -20,9 +41,39 @@ while [ -n "$1" ]; do
     case $1 in
             -l|-list|--list)
                 list=$2
-                shift ;;
+                shift 2
+		continue ;;
 
-             *) echo -e "${RED} ${BOLD}"
+	    -g|-git|--git)
+		git=true
+		shift
+		continue ;;
+
+	    -b|-bazaar|--bazaar)
+		bazaar=true
+		shift
+		continue ;;
+
+	    -m|-mercury|--mercury)
+		mercury=true
+		shift
+		continue ;;
+
+	    -s|-svn|--svn)
+		svn=true
+		shift
+		continue ;;
+
+	    -a|-all|--all)
+		all=true
+		shift
+		continue ;;
+
+	    -h|-help|--help)
+            	printusage
+           	shift ;;
+
+             *) echo -e "${RED}"
                 echo -e "Invalid Flag: $1"
 		printusage
 		exit
@@ -40,55 +91,61 @@ if [ ! -n "$list" ]; then
 fi
 
 
-echo -e "${BOLD}${PURPLE}Scanning for exposed git folders (1/4)${NC}"
+if [ $git ] || [ $all ]; then
+echo -e "${PURPLE}Scanning for exposed git folders${NC}"
+	for targetna in $(cat $list); do
+		if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.git/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
+			echo -e "${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
+		fi
+
+		if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.git/" ) =~ 'Index of' ]]; then
+			echo -e "${RED}[+] VULN: ${NC}${targetna}"
+		fi
+	done
+	echo ""
+fi
+
+
+if [ $bazaar ] || [ $all ]; then
+echo -e "${PURPLE}Scanning for exposed Bazaar folders${NC}"
 for targetna in $(cat $list); do
-	if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.git/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
-		echo -e "${BOLD}${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
-	fi
+		if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.bzr/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
+			echo -e "${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
+		fi
 
-
-	if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.git/" ) =~ 'Index of' ]]; then
-		echo -e "${BOLD}${RED}[+] VULN: ${NC}${targetna}"
-	fi
-done
-
+		if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.bzr/" ) =~ 'Index of' ]]; then
+			echo -e "${RED}[+] VULN: ${NC}${targetna}"
+		fi
+	done
 echo ""
-echo -e "${BOLD}${PURPLE}Scanning for exposed Bazaar folders (2/4)${NC}"
+fi
+
+
+if [ $mercury ] || [ $all ]; then
+echo -e "${PURPLE}Scanning for exposed Mercury folders${NC}"
 for targetna in $(cat $list); do
-	if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.bzr/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
-		echo -e "${BOLD}${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
-	fi
+		if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.hg/hgrc/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
+			echo -e "${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
+		fi
 
-
-	if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.bzr/" ) =~ 'Index of' ]]; then
-		echo -e "${BOLD}${RED}[+] VULN: ${NC}${targetna}"
-	fi
-done
-
-
+		if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.hg/hgrc/" ) =~ '[paths]' ]]; then
+			echo -e "${RED}[+] VULN: ${NC}${targetna}"
+		fi
+	done
 echo ""
-echo -e "${BOLD}${PURPLE}Scanning for exposed Mercury folders (3/4)${NC}"
+fi
+
+
+if [ $svn ] || [ $all ]; then
+echo -e "${PURPLE}Scanning for exposed svn folders${NC}"
 for targetna in $(cat $list); do
-	if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.hg/hgrc/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
-		echo -e "${BOLD}${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
-	fi
+		if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.svn/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
+			echo -e "${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
+		fi
 
-
-	if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.hg/hgrc/" ) =~ '[paths]' ]]; then
-		echo -e "${BOLD}${RED}[+] VULN: ${NC}${targetna}"
-	fi
-done
-
-
+		if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.svn/" ) =~ 'Index of' ]]; then
+			echo -e "${RED}[+] VULN: ${NC}${targetna}"
+		fi
+	done
 echo ""
-echo -e "${BOLD}${PURPLE}Scanning for exposed svn folders (4/4)${NC}"
-for targetna in $(cat $list); do
-	if [[ $(curl -s -m 3 -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0" "${targetna}/.svn/" -w %{http_code} -o /dev/null ) =~ '403' ]]; then
-		echo -e "${BOLD}${YELLOW}[+] MAYBE VULN: ${NC}${targetna}"
-	fi
-
-
-	if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${targetna}/.svn/" ) =~ 'Index of' ]]; then
-		echo -e "${BOLD}${RED}[+] VULN: ${NC}${targetna}"
-	fi
-done
+fi
