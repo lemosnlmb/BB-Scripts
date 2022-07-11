b6cfb23b68ed949c2b85c8c51c7b31898cf4c682
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 15:08:26 +0100

    Update ssrf-params.sh

--- ssrf/ssrf-params/ssrf-params.sh
+++ ssrf/ssrf-params/ssrf-params.sh
@@ -7,5 +7,10 @@ NC='\033[0m'
 
 list=$1
 
-cat $list | grep "=" | qsreplace "FUZZ" > $list.tmp && mv $list.tmp $list
-interlace -tL $list -c "ffuf -v -r -w payloads.txt -u \"_target_\" -mr \"compute.internal\" " 2>/dev/null | grep "| URL |" | awk '{print $4}' | while read url; do echo -e "${RED}Vulnerable to SSRF${NC}: $url"; done
+cat $list | grep "=" | qsreplace "FUZZ" > $list.tmp
+interlace -tL $list.tmp -c "ffuf -r -w payloads.txt -u \"_target_\" -mr \"compute.internal\" " 2>/dev/null | grep "| URL |" | awk '{print $4}' | while read url
+do 
+  echo -e "${RED}Vulnerable to SSRF${NC}: $url"
+done
+
+rm -rf $list.tmp
